dccd2377b75695730d42eca2d196746df2c8b4f7
Daniel Marton <instantbuddha@gmail.com>
2022-02-07 10:56:21 +0100

    Initiate react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainWrapper from './components/MainWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <MainWrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainWrapper.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class MainWrapper extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         
+      }
+    }
+    
+  render() {
+    return <div>
+        <h1>Finnhub Stocks</h1>
+    </div>
+  }
+}
+
+export default MainWrapper
