9138f6f362f46a59e22edbcfb3c481b9ac43e7e8
Daniel Marton <instantbuddha@gmail.com>
2022-02-28 10:34:58 +0100

    SymbolLister downloads and logs symbols data for XNYS

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,17 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-
-const apiUrlParts = {
-  base: "https://finnhub.io/api/v1",
-  stockSymbols: "/stock/symbol?exchange=US&token=",
-  token: "c1mrjdi37fktai5sgaog",
-  filterNY: "&mic=XNYS"
-}
 
 class Home extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         
-      }
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
     }
-
-    
-
-    async componentDidMount() {
-      const testUrl = apiUrlParts.base + apiUrlParts.stockSymbols + apiUrlParts.token + apiUrlParts.filterNY
-
-      await axios.get(testUrl)
-          .then(response => {
-              console.log(response)
-          })
-          .catch(error => { console.log(error) })
-
   }
 
-    
   render() {
     return <div>
-        <h1>Finnhub Stocks</h1>
+      <h1>Finnhub Stocks</h1>
     </div>
   }
 }
--- src/components/symbol-list/SymbolLister.js
+++ src/components/symbol-list/SymbolLister.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
+const apiUrlParts = {
+  base: "https://finnhub.io/api/v1",
+  stockSymbols: "/stock/symbol?exchange=",
+  token: "&token=c1mrjdi37fktai5sgaog"
+  
+}
 
 class SymbolLister extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      stockData: []
+    }
+
+    this.getJSON = this.getJSON.bind(this)
+  }
+
+  async componentDidMount() {
+    const country = "US"
+    const filterNY = "&mic=XNYS"
+    const stockSymbolsUrl = `${apiUrlParts.base}${apiUrlParts.stockSymbols}${country}${apiUrlParts.token}${filterNY}`
     
+
+    this.getJSON(stockSymbolsUrl)
+  }
+
+  async getJSON(url) {
+    await axios.get(url)
+      .then(response => {
+        let copiedTempState = { ...this.state }
+        copiedTempState.stockData = response.data
+        copiedTempState.isReady = true
+        this.setState(copiedTempState)
+      })
+      .catch(error => { console.log(error) })
+  }
+
   render() {
-    return (
-      <h1>SymbolLister</h1>
+    console.log(this.state.stockData)
+    return (<div>
+               <h1>SymbolLister</h1>
+               {this.state.stockData.length > 0 ?
+                <p>ready</p> : <p>Downloading data</p>}
+
+    </div>
+     
     )
   }
 }
